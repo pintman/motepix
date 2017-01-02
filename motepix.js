79557337ab28495d2ed608e1515824db0ad36f40
Marco Bakera <marco@bakera.de>
2017-01-02 11:01:09 +0100

    some more js tests.

--- motepix.js
+++ motepix.js
@@ -1,10 +1,18 @@
+function index_loaded()
+{
+	alert("index_loaded");
+}
 
 $(document).ready(
 	function() {
+		$("#status").addClass("test");
 		$("a").click(
 			function()
 			{
-				alert("so");
+				//alert("so");
+				//$.get("index.htm", index_loaded);
+				$("#status").text("Neuer Status");
 			});
 	}
 );
+
