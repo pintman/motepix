2337c7473351f9bfc5d89d3c6ddefd9d65116ebd
Marco Bakera <marco@bakera.de>
2017-01-02 10:51:33 +0100

    init

---
+++ motepix.py
@@ -0,0 +1,9 @@
+"""Ein server für motepix clients."""
+
+class MotepixServer:
+    def __init__(self, port=55443):
+        """Startet einen motepix server."""
+
+
+if __name__ == "__main__":
+    MotepixServer()
