751ea4c64d4c6d148197378af5d6d1a135a7d50a
Marco Bakera <marco@bakera.de>
2017-01-03 12:59:41 +0100

    cleanup.

--- static/motepix.js
+++ static/motepix.js
@@ -3,29 +3,23 @@ $(document).ready(
 	function() {
 		// console.log("document ready");
 		ask_for_data();
-		//$.get("/pixel_color", data_received);
-		
-		/*
-		$("a").click(
-			function()
-			{
-				//alert("so");
-				//$.get("index.htm", index_loaded);
-				$("#status").text("Neuer Status");
-			});
-		*/
 	}
 );
 
 function ask_for_data()
 {
 	//console.log("ask for data");
-	$.get("/pixel_color", data_received);
+	console.log(window.location.href);
+	// TODO get coordinates from location url
+	var x = 0
+	var y = 0
+	$.get("/px/"+x+"/"+y, data_received);
 }
 
 function data_received(response)
 {
 	// console.log("data_received:" + response);
 	$("#pixel").attr("style", "width:100px;height:100px;background-color:" + response);
-	window.setTimeout(ask_for_data, 100);
+	// wait for some ms and ask for data again
+	window.setTimeout(ask_for_data, 1000);
 }
