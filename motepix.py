6ddefbe6c4291c95794a7d8303a5be111e7b98fc
Marco Bakera <marco@bakera.de>
2017-01-03 13:43:10 +0100

    using threading work for background work.

--- motepix.py
+++ motepix.py
@@ -1,28 +1,34 @@
 """Ein server für motepix clients."""
 
 import bottle
+import threading
+import time
 
 on_off = [[False, False, False]]
 
+
 def color_at(x, y):
     """Return the color at the given position."""
+    global on_off
 
     if on_off[x][y]:
         return "grey"
     else:
         return "black"
 
+def swap_all_colors():
+    global on_off
+    for x in range(len(on_off)):
+        for y in range(len(on_off[0])):
+            on_off[x][y] = not on_off[x][y]
+
 @bottle.route("/show/<x:int>/<y:int>")
 def show(x, y):
     global on_off
-    on_off[x][y] = not on_off[x][y]
-    return bottle.template("show", status=on_off[x][y], title=str(x)+"|"+str(y))
-
+    return bottle.template("show", status=color_at(x, y), title=str(x)+"|"+str(y))
 
 @bottle.route("/px/<x:int>/<y:int>")
 def px_color(x, y):
-    global on_off
-    on_off[x][y] = not on_off[x][y]
     return color_at(x, y)
 
 @bottle.route("/static/<filename>")
@@ -30,8 +36,19 @@ def serve_static(filename):
     """Serving static filed like js, css or images."""
     return bottle.static_file(filename, root="./static")
 
+def worker():
+    """Worker Thread that is changing the display data."""
+    global on_off
+    while True:
+        swap_all_colors()
+        time.sleep(2)
+
 
 if __name__ == "__main__":
     # TODO Add parameter for ip
+
+    th = threading.Thread(target=worker)
+    th.start()
+
     bottle.run(host="192.168.178.49", port=8088, debug=True, reloader=True)
     
--- static/motepix.js
+++ static/motepix.js
@@ -8,21 +8,20 @@ $(document).ready(
 
 function ask_for_data()
 {
-	//console.log("ask for data");
-	console.log(window.location.href);
 	// in 'http://192.168.178.49:8088/show/0/0' fetch part after show
 	// and split the coordinates afterwards
 	var x_y = window.location.href.split("/show/")[1].split("/");
 	var x = x_y[0];
 	var y = x_y[1];
-	//console.log(x_y + " " + x + "|" + y);
+	// fetch pixel data async.
 	$.get("/px/"+x+"/"+y, data_received);
 }
 
 function data_received(response)
 {
 	// console.log("data_received:" + response);
-	$("#pixel").attr("style", "position:absolute;top:0px;bottom:0px;width:99%;background-color:" + response);
+	var style = "position:absolute;top:0px;bottom:0px;width:99%";
+	$("#pixel").attr("style", style + ";background-color:" + response);
 	// wait for some ms and ask for data again
-	window.setTimeout(ask_for_data, 1000);
+	window.setTimeout(ask_for_data, 10);
 }
